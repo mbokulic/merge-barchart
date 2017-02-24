2ecf9a90b96f394137920df8b65b4ace612198b3
marko <bokmarko@gmail.com>
2017-02-24 17:25:21 +0100

    cleaned up event_publisher code

--- js/Event_publisher.js
+++ js/Event_publisher.js
@@ -22,24 +22,3 @@ Event_publisher.prototype.unsubscribe = function(object) {
 Event_publisher.prototype.purge_all = function(event) {
     this.handlers = [];
 };
-
-var inner_loop = 1;
-var outer_loop = 1;
-
-event_publisher_helpers = {
-    subscribe_eachother: function(elements, method_name) {
-        for (var i = elements.length - 1; i >= 0; i--) {
-            outer_loop ++;
-            var notifier = elements[i];
-            for (var j = elements.length - 1; j >= 0; j--) {
-                inner_loop ++;
-                var subscriber = elements[j];
-                if(i === j) {
-                    continue;
-                };
-                notifier.subscribe(subscriber[method_name], subscriber);
-            };
-        };
-    }
-};
-
